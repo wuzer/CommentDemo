bac0a39a46fc7f57aff5d4b802830e516dbfa484
Jefferson <yuanwu.zon@gmail.com>
2016-08-27 10:16:41 +0800

    创建工程

---
+++ CommentDemo/CommentDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CommentDemo
+//
+//  Created by Jefferson on 16/8/27.
+//  Copyright © 2016年 Jefferson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CommentDemo/CommentDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CommentDemo
+//
+//  Created by Jefferson on 16/8/27.
+//  Copyright © 2016年 Jefferson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
